feedaa091a8d5cceef6a2b5798ff76704dfa677d
sathishmepco53@gmail.com <sathish@greenboards.in>
2018-04-23 00:14:30 +0530

    committing source code

---
+++ src/com/cdac/calc/Calculator.java
@@ -0,0 +1,13 @@
+package com.cdac.calc;
+
+public class Calculator {
+	
+	public int sum(int a,int b){
+		return a+b;
+	}
+	
+	public int sub(int a,int b){
+		return a-b;
+	}
+
+}
---
+++ src/com/cdac/calc/Main.java
@@ -0,0 +1,10 @@
+package com.cdac.calc;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.sum(10, 20));
+	}
+}
